412567fa8d6968597d15073ca9512de3de5cd669
renxiaoyao123 <375188586@qq.com>
2019-06-20 13:39:39 +0800

    登录页完成

---
+++ src/components/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <h2>wosh</h2> -->
+  <div class="login"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.login {
+  width: 100%;
+  height: 100%;
+  background-color: #324152;
+}
+</style>
